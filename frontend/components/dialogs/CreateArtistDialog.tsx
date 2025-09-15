ad005e43f6b7ddc6452f70c05ed8eb5abe2e1a02
kavindu-udara <karunasinghesampath38@gmail.com>
2025-09-16 03:43:49 +0530

    added react hot source for alerting. create add new artist submit funvion

--- frontend/components/dialogs/CreateArtistDialog.tsx
+++ frontend/components/dialogs/CreateArtistDialog.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject } from 'react';
+import React, { RefObject, useRef, useState } from 'react';
 import {
     Dialog,
     DialogContent,
@@ -11,8 +11,63 @@ import { Label } from '@radix-ui/react-label';
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
 import { Button } from '../ui/button';
+import toast from 'react-hot-toast';
+import apiClient from '@/axios/apiClient';
 
-const CreateArtistDialog = ({triggerBtnRef} : {triggerBtnRef : RefObject<HTMLButtonElement | null>}) => {
+type FormType = {
+    name: string,
+    description: string
+}
+
+const CreateArtistDialog = ({ triggerBtnRef }: { triggerBtnRef: RefObject<HTMLButtonElement | null> }) => {
+
+    const artistImageInputRef = useRef<HTMLInputElement>(null);
+
+    const [form, setForm] = useState<FormType>({
+        name: "",
+        description: ""
+    })
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async () => {
+        if (!form.name || !form.description) {
+            toast.error("All fields are required");
+            return;
+        }
+
+        const image = artistImageInputRef.current?.files?.[0];
+        if (!image) {
+            toast.error("Image is required")
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("name", form.name);
+        formData.append("description", form.description);
+        formData.append("image", image);
+
+        apiClient.post("/",
+            formData
+            , {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        ).then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.error(err);
+        });
+
+    }
+
     return (
         <Dialog>
             <DialogTrigger ref={triggerBtnRef} className='hidden'>Open</DialogTrigger>
@@ -25,12 +80,12 @@ const CreateArtistDialog = ({triggerBtnRef} : {triggerBtnRef : RefObject<HTMLBut
                 </DialogHeader>
                 <div className='flex flex-col gap-3'>
                     <Label>Name</Label>
-                    <Input/>
+                    <Input name='name' type='text' value={form.name} onChange={handleInputChange} />
                     <Label>Image</Label>
-                    <Input/>
+                    <Input name='image' type='file' accept='image/*' ref={artistImageInputRef} />
                     <Label>Description</Label>
-                    <Textarea/>
-                    <Button>Create</Button>
+                    <Textarea name='description' value={form.description} onChange={handleTextAreaChange} />
+                    <Button onClick={handleSubmit}>Create</Button>
                 </div>
             </DialogContent>
         </Dialog>
