78573f6510f962c8c81c1166aa76753b458a790e
kavindu-udara <karunasinghesampath38@gmail.com>
2026-04-19 00:51:30 +0530

    created a auth initializer component and changed user layout to use zustand

---
+++ webfront/components/initializers/auth-initializer.tsx
@@ -0,0 +1,19 @@
+"use client";
+import apiClient from '@/lib/axios';
+import useAuthStore from '@/lib/store';
+import React, { use, useEffect } from 'react'
+
+const AuthInitializer = () => {
+    useEffect(() => {
+        apiClient.get("/auth/account").then((response) => {
+            useAuthStore.setState({ user: response.data.account });
+        }).catch((error) => {
+            useAuthStore.setState({ user: null });
+            console.error("Failed to fetch account info:", error.response?.data || error.message);
+        });
+    });
+
+    return null;
+}
+
+export default AuthInitializer
--- webfront/components/layouts/user-layout.tsx
+++ webfront/components/layouts/user-layout.tsx
@@ -1,20 +1,19 @@
 "use client";
-import apiClient from '@/lib/axios';
+import useAuthStore from '@/lib/store';
 import { useRouter } from 'next/navigation';
 import React, { ReactNode, useEffect } from 'react'
 
 const UserLayout = ({ children }: { children: ReactNode }) => {
 
+    const user = useAuthStore((state) => state.user);
+
     const router = useRouter();
 
     useEffect(() => {
-        apiClient.get("/auth/account").then((response) => {
-            console.log("Account info:", response.data);
-        }).catch((error) => {
-            console.error("Failed to fetch account info:", error.response?.data || error.message);
-            router.push("/login?errorMessage=Please log in to access your account");
-        });
-    }, [router]);
+        if (!user) {
+            router.push("/login?errorMessage=Please login to access this page");
+        }
+    }, [user, router]);
 
     return (
         <>
